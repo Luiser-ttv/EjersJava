57e8ee2b58e0cb9c6178499ed4788fb2437175a7
rioja <rioja@DESKTOP-LRVG3CG>
2019-11-28 16:54:05 +0100

    Versión inicial con el primer ejercicio hecho.

---
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosbasicosjava;
+
+
+
+/**
+ *
+ * @author rioja
+ */
+public class EjerciciosBasicosJava {
+
+    public boolean fiestaArdillas(int numBellotas, boolean finDeSemana){
+        if(finDeSemana){
+    return true;
+}
+if(numBellotas >= 40 && numBellotas <= 60 ){
+    return true;
+}
+return false;
+    }
+    
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EjerciciosBasicosJava ejercicios=new EjerciciosBasicosJava();
+        System.out.print(ejercicios.fiestaArdillas(30, false));
+        System.out.print(ejercicios.fiestaArdillas(50, false));
+        System.out.print(ejercicios.fiestaArdillas(70, true));
+    }
+    
+}
